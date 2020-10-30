5e7d300dcb33720348633811f73ceacf7ac236c9
jennalehn <jenna.lehn@gmail.com>
2020-10-30 15:38:19 -0400

    graphs are fun

--- Ex10.R
+++ Ex10.R
@@ -2,7 +2,30 @@
 
 # Usage:
 
-# 1. find excel data like gpa or major idk make plot
+# 1. found excel data of college GPA and SAT scores 
+# and make scatter plot
 
+#read in file
+satgpa=read.csv("satandgpa.csv",header=TRUE)
+head(satgpa)
 
-# 2. 
+# get ggplot library
+library(ggplot2)
+
+# make scatter plot with trend line
+ggplot(data=satgpa,aes(x=SAT.Score,y=College.GPA))+
+  # make scatter plot
+  geom_point(color="turquoise")+
+  # make trend line
+  stat_smooth(method="lm")+
+  ylab("College GPA")+
+  xlab("SAT Score")+
+  theme_classic()
+
+# 2. write script for data.txt to make 2 figures
+
+# read in data.txt
+data=read.csv(file="data.txt",header=TRUE)
+head(data)
+
+# barplot of means of four populations
