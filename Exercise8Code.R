1ab0ee2f59c505d61d7a5a3a0ac004625ed68221
Jordan Brown <jbrown36@nd.edu>
2020-10-31 14:05:13 -0400

    Specified code to export graph

--- Exercise8Code.R
+++ Exercise8Code.R
@@ -2,14 +2,20 @@
 #Challenge 1
 #load packages
 library(ggplot2)
-library(cowplot)
 #load text file
 plants= read.table("Plant_height.txt", header=TRUE, sep="\t", stringsAsFactors=FALSE)
 #produce a scatter plot of temperature (temp) vs plant height (height)
-ggplot(data = plants,aes(x = temp, y = height)) +
-  geom_point(color = "blue", shape = 19, size = 2) +
-  stat_smooth(method="lm") +
-  xlab("Temperature (°C)") + 
-  ylab("Plant Height (m)") +
-  theme_classic()
-#includes trendline
+#Store graph as variable to be exported
+Plantfig1 <- ggplot(data = plants,aes(x = temp, y = height)) + #specify dataset and variables
+  geom_point(color = "blue", shape = 15, size = 2) + #specifies data points appearance
+  stat_smooth(method="lm", color ="black") + #black trendline
+  xlab("Temperature (°C)") + #add x axis label
+  ylab("Plant Height (m)") + #add y axis label
+  theme_classic() #remove gray background and gridline
+
+#save graph to an external file
+ggsave(filename = "Plantfig1.pdf",
+       plot = Plantfig1,
+       width = 8,
+       height = 5,
+       dpi = 300)
