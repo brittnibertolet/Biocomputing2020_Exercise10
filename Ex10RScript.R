1013e0d3d5d08bd0d6d38e6d0478d1dc11f3bc55
Michelle Huang <o.omichelley@gmail.com>
2020-10-31 09:18:23 -0400

    combined into function; ran function

--- Ex10RScript.R
+++ Ex10RScript.R
@@ -9,9 +9,19 @@ dogweights <- read.csv("~/Desktop/Bioinformatics/Biocomputing2020_Exercise10/dog
 library(ggplot2)
 ggplot(dogweights, aes(x=age, y=weight, color=breed))+
   geom_point()+
+  # add in trend line (one for both breeds, instead of one overall trend line)
   stat_smooth()+
   theme_classic()
 
-# create function
+# combine into a function
+scatter <- function(i){
+  file <- read.csv(i, header=TRUE, stringsAsFactors = FALSE)
+  ggplot(file, aes(x=age, y=weight, color=breed))+
+    geom_point()+
+    stat_smooth()+
+    theme_classic() 
+}
 
+# run the function
+scatter("~/Desktop/Bioinformatics/Biocomputing2020_Exercise10/dogweights.csv")
   
