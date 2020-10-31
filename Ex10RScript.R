1a134f16a72f5008b78341523ecf31bfb89cb318
Michelle Huang <o.omichelley@gmail.com>
2020-10-31 09:10:57 -0400

    read in more applicable data

--- Ex10RScript.R
+++ Ex10RScript.R
@@ -2,11 +2,16 @@
 ## of two variables with a trend line
 
 # read in file
-lutz <- read.csv("~/Desktop/Bioinformatics/Biocomputing2020_Exercise10/lutz.csv",
+dogweights <- read.csv("~/Desktop/Bioinformatics/Biocomputing2020_Exercise10/dogweights.csv",
                  header=TRUE, stringsAsFactors = FALSE)
 
 # create scatterplot
 library(ggplot2)
-ggplot(lutz, aes(x=Ct, y=Tm))+
-  geom_jitter()+
+ggplot(dogweights, aes(x=age, y=weight, color=breed))+
+  geom_point()+
+  stat_smooth()+
   theme_classic()
+
+# create function
+
+  
