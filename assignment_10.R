2b9a0144b423620008dd6ada4bc31b317f2d4c9f
Kevin Catalfano <kevin.catalfano@gmail.com>
2020-11-04 18:40:15 -0500

    notes on code

---
+++ assignment_10.R
@@ -0,0 +1,18 @@
+# exercise 10
+# 1. These data can come from your own research, your daily life, or the 
+# internet. Enter those data into a text file or Excel and then save a text 
+# file, and write a script that loads this text file and produces a scatter 
+# plot of those two variables that includes a trend line.
+
+
+
+
+# 2. Given the data in “data.txt”. Write a script that generates two 
+##figures that sumamrize the data. 
+###First, show a barplot of the means of the four populations. 
+###Second, show a scatter plot of all of the observations. 
+##You may want to “jitter” the points (geom_jitter()) to make it easier to 
+##see all of the observations within a population in your scatter plot. 
+##Alternatively, you could also try setting the alpha argument in 
+##geom_scatterplot() to 0.1. Do the bar and scatter plots tell you different 
+##stories? Why?
