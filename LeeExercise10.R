62163c127e97578c37f7d938f428f5e24a438d27
Davin Lee <dlee27@crcfe02.crc.nd.edu>
2020-11-06 00:41:05 -0500

    problem 2

--- LeeExercise10.R
+++ LeeExercise10.R
@@ -6,7 +6,7 @@
 
 ## Steps
 # load data with read.csv()
-# load ggplot 
+# load ggplot2 
 # scatter plot, add data for both variables
 # add axis labels
 # add trend line
@@ -26,5 +26,55 @@ ggplot(data = hungrykitchen, aes(x=Hungry, y=Kitchen)) +
 # Barplot of means of the four populations
 # Scatter plot of all observations (use geom_jitter())
 # Do the plots telll you different stories? Why?
+
+## Steps
+# load data.txt
+# load ggplot2
+# get means of each population and store in variables
+#   make vectors for regions
+#   for loop to iterate through data
+#   if else statement for region, then append into separate vectors
+#   calculate mean for each region and store in variable
+# make barplot with means
+# scatter of all observations using jitter
+
+data<-read.csv("Biocomputing2020_Exercise10/data.txt")
+library(ggplot2)
+
+# vectors with all observations by region
+north<-c()
+south<-c()
+east<-c()
+west<-c()
+
+for (i in 1:nrow(data)){
+  if (data$region[i]=="north"){
+    north<-append(north, data$observations[i])
+  }else if (data$region[i]=="south"){
+    south<-append(south, data$observations[i])}
+  else if (data$region[i]=="east"){
+    east<-append(east, data$observations[i])}
+  else{ west<-append(west, data$observations[i])}
+}
+
+means<-c(mean(north), mean(south), mean(east), mean(west))
+names<-c("north", "south", "east", "west")
+
+# data frame for means
+meansDF<-data.frame("Region" = names, "Mean" = means)
+
+# bar plot of means
+ggplot(meansDF, aes(x = Region)) +
+  geom_bar() +
+  theme_bw() +
+  xlab("Region") 
+
+# scatter of all observations
+ggplot(data, aes(x = region, y = observations)) +
+  geom_jitter()
+
+# The plots tell different stories. 
+# Bar makes all regions look the same
+# Scatter shows more distribution within region for some
 
 
