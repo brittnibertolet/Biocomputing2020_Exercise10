53103c6c29e7207d185422d41d6add8581e254c0
Davin Lee <dlee27@crcfe02.crc.nd.edu>
2020-11-05 23:25:35 -0500

    problem set up

---
+++ LeeExercise10.R
@@ -0,0 +1,23 @@
+#### Exercise 10 ####
+
+### Problem 1
+# Make data text file then write script that loads text file 
+#     and produces scatter plot that includes trend line
+
+## Steps
+# load data with read.csv()
+# load ggplot 
+# scatter plot
+# add data for second variable
+# color code variables
+# add axis labels
+# add trend line
+
+
+### Problem 2
+# Write script for two figures to summarize "data.txt"
+# Barplot of means of the four populations
+# Scatter plot of all observations (use geom_jitter())
+# Do the plots telll you different stories? Why?
+
+
